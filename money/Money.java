ef5858020132210d922e1270652567ecdeff49ee
alek-3 <jaianism.of.grotius@hotmailco.jp>
2017-11-01 22:12:45 +0900

    11章まで

--- money/Money.java
+++ money/Money.java
@@ -1,11 +1,30 @@
 package money;
 
-public class Money {
-	protected int amount; // "Protected" enables child class to refer to
+class Money {
+	protected int amount; // "Protected" enables only child class to refer to
+	protected String currency;
+
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	Money times(int muliplier) {
+		return new Money(amount * muliplier, currency);
+	};
+
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount;
-}
+		return amount == money.amount
+				&& currency.equals(money.currency);
+	}
 
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
 
 }
